0b26c7f198e56a08322f898fd7d3ff83aed2a5f2
SofieHedemann <sofhedemann@gmail.com>
2023-02-09 09:35:24 +0100

    :+1:

---
+++ FriendsToMeet/main.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
